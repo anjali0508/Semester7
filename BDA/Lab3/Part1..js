c95e6e9a9a75d5026125fff26892e2f013389288
nairanjali0508 <nairanjali0508@gmail.com>
2020-10-06 13:27:51 +0530

    Lab3

---
+++ BDA/Lab3/Part1..js
@@ -0,0 +1,15 @@
+db.createCollection('Student')
+db.Student.insert({ RollNo:1, Age:20, Contact:9988778899, Name:"Anjali", Email:"anjali.cs17@bmsce.ac.in"})
+db.Student.insert({ RollNo:2, Age:22, Contact:9988112233, Name:"Adh", Email:"adh.cs17@bmsce.ac.in"})
+db.Student.insert({ RollNo:3, Age:21, Contact:9988554455, Name:"Apek", Email:"apek.cs17@bmsce.ac.in"})
+
+db.Student.update({RollNo:1},{$set:{Email:"aditya.cs17@bmsce.ac.in"}})
+db.Student.update({RollNo:2},{$set:{Name:"Rekha"}})
+
+
+db.Student.find().pretty()
+
+
+
+
+
---
+++ BDA/Lab3/Part2.js
@@ -0,0 +1,29 @@
+use Database
+db.createCollection('Customer')
+db.Customer.insert({cust_id:1,Acc_bal:1500,Acc_type:"Z"})
+db.Customer.insert({cust_id:2,Acc_bal:3000,Acc_type:"A"})
+db.Customer.insert({cust_id:1,Acc_bal:1200,Acc_type:"A"})
+db.Customer.insert({cust_id:3,Acc_bal:500,Acc_type:"Z"})
+db.Customer.insert({cust_id:2,Acc_bal:1600,Acc_type:"Z"})
+db.Customer.find().pretty()
+
+db.Customer.aggregate([
+    {
+        $group: {
+            _id: "$cust_id",
+            min_bal: {$min: "$Acc_bal"},
+            max_bal: {$max: "$Acc_bal"}
+        }
+    }
+]);
+
+db.Customer.find({Acc_bal:{$gt:1200}, Acc_type:"Z"})
+
+
+
+
+
+
+
+
+
