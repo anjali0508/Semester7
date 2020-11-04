c15416badb8d4674c32ddf0aded064142934ac12
nairanjali0508 <nairanjali0508@gmail.com>
2020-11-04 15:11:24 +0530

    Lab7

--- DSR/Lab7/Lab7.R
+++ DSR/Lab7/Lab7.R
@@ -3,4 +3,5 @@ dotchart(mtcars$mpg, labels = row.names(mtcars),cex = 0.6, xlab = "mpg")
 
 library(gcookbook)
 library(ggplot2)
-ggplot(cabbage_exp, aes(x=Date, y=Weight, fill=Cultivar)) +geom_bar(stat="identity", position="dodge") +geom_text(aes(label=Weight), vjust=1.5, colour="white", position=position_dodge(.9), size=3)
+df = cabbage_exp
+barplot(Weight ~ Cultivar+Date, data=df, xlab = "Date", ylab = "weight", beside=TRUE, legend = TRUE, col=c("turquoise3","turquoise1"))
